4988e43b742850b895d961c4f7bedae127ad74b6
Tiffany Lamm <tiffany.lamm091@myci.csuci.edu>
2019-03-04 15:14:53 -0800

    new changes

---
+++ TaskR/app/src/main/java/android/bignerdranch/taskr/NonFriendProfile.java
@@ -0,0 +1,13 @@
+package android.bignerdranch.taskr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NonFriendProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_non_friend_profile);
+    }
+}
