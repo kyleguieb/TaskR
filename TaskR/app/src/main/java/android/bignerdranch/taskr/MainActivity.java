de3257d4a93b14079889cfbd7f9e9075e53e7289
Tiffany Lamm <tiffany.lamm091@myci.csuci.edu>
2019-03-06 18:15:38 -0800

    Login and DM buttons work

--- TaskR/app/src/main/java/android/bignerdranch/taskr/MainActivity.java
+++ TaskR/app/src/main/java/android/bignerdranch/taskr/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    private ImageButton button; // invisible_calendar_button, invisible_profile_button;
+    private ImageButton buttons; // invisible_calendar_button, invisible_profile_button;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,48 +45,20 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        button = (ImageButton) findViewById(R.id.Direct);
-        button.setOnClickListener(new View.OnClickListener() {
+        buttons = findViewById(R.id.DirectBtn);
+        buttons.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
                 directMessageScreen();
             }
         });
-//        invisible_calendar_button = findViewById(R.id.invisibleCalBtn);
-//        invisible_calendar_button.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v)
-//            {
-//                calendarScreen();
-//            }
-//        });
-//        invisible_profile_button = findViewById(R.id.invisibleProfileBtn);
-//        invisible_profile_button.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v)
-//            {
-//                profileScreen();
-//            }
-//        });
     }
 
     public void directMessageScreen()
     {
         //startActivity(new Intent(MainActivity.this,Login.class));
-        Intent intent = new Intent(this, DirectMessageChat.class);
+        Intent intent = new Intent(this, DirectMessagesList.class);
         startActivity(intent);
     }
-
-//    public void calendarScreen()
-//    {
-//        //startActivity(new Intent(MainActivity.this,Login.class));
-//        Intent intent2 = new Intent(this, Calendar.class);
-//        startActivity(intent2);
-//    }
-//
-//    public void profileScreen()
-//    {
-//        //startActivity(new Intent(MainActivity.this,Login.class));
-//        Intent intent3 = new Intent(this, Calendar.class);
-//        startActivity(intent3);
-//    }
 
 }
