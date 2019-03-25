e3425bf3a8713edcc215456cdc7d22f479e0df3e
tlrYere <tyler.yere772@myci.csuci.edu>
2019-03-25 16:14:40 -0700

    added Task class

---
+++ TaskR/app/src/main/java/android/bignerdranch/taskr/Task.java
@@ -0,0 +1,43 @@
+package android.bignerdranch.taskr;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+    private String mName;
+    private String mDescription;
+    private LocalDateTime mDateAndTimeDue;
+    //private LocalDateTime dateAndTimeForNotification;   //this is the date and time before the task's due time that sends a popup notification to the user's phone
+    //To be figured out later
+
+    public Task(String taskName, String taskDescription, LocalDateTime taskDateAndTimeDue)
+    {
+        mName = taskName;
+        mDescription = taskDescription;
+        mDateAndTimeDue = taskDateAndTimeDue;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public LocalDateTime getmDateAndTimeDue() {
+        return mDateAndTimeDue;
+    }
+
+    public void setmDateAndTimeDue(LocalDateTime mDateAndTimeDue) {
+        this.mDateAndTimeDue = mDateAndTimeDue;
+    }
+}
