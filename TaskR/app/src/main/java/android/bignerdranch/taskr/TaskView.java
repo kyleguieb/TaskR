9f964a267926c850fac726354b7e7f18d06f6d26
hiimkoi <hithereimkoi@gmail.com>
2019-04-05 19:13:25 -0700

    minor change that helps the edit button from tyler's implementation

--- TaskR/app/src/main/java/android/bignerdranch/taskr/TaskView.java
+++ TaskR/app/src/main/java/android/bignerdranch/taskr/TaskView.java
@@ -6,10 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-<<<<<<< HEAD
-
-=======
->>>>>>> Tyler
 
 public class TaskView extends AppCompatActivity {
 
@@ -19,7 +15,7 @@ public class TaskView extends AppCompatActivity {
         setContentView(R.layout.activity_task_view);
 
         defineButtons();
-<<<<<<< HEAD
+
     }
 
     private void defineButtons(){
@@ -32,26 +28,10 @@ public class TaskView extends AppCompatActivity {
         public void onClick(View v) {
             switch(v.getId()) {
                 case R.id.deleteBtn:
+                    //delete from the database using method defined in Main Activity
+                    startActivity(new Intent(TaskView.this, MainActivity.class));
+                    break;
 
-                case R.id.editBtn:
-
-            }
-        }
-    };
-    }
-
-=======
-    }
-
-    public void defineButtons() {
-        findViewById(R.id.editBtn).setOnClickListener(buttonClickListener);
-        findViewById(R.id.deleteBtn).setOnClickListener(buttonClickListener);
-    }
-
-    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId())  {
                 case R.id.editBtn:
                     EditText inputName = findViewById(R.id.TitleView_editText);
                     EditText inputDescription = findViewById(R.id.DescriptionView_editText);
@@ -62,10 +42,12 @@ public class TaskView extends AppCompatActivity {
                             inputDescription.getText().toString().equals("") ||
                             inputTime.getText().toString().equals("") ||
                             inputDate.getText().toString().equals("")) {
-                                Toast.makeText(TaskView.this, "Please fill in all fields!",
+                        Toast.makeText(TaskView.this, "Please fill in all fields!",
                                 Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(TaskView.this, MainActivity.class));
-                                //if not all fields are filled, restart TaskView activity
+                        break;
+                        //shouldn't go back to main activity if all fields aren't completed
+                        //startActivity(new Intent(TaskView.this, MainActivity.class));
+                        //if not all fields are filled, restart TaskView activity
                     }
 
                     Task alteredTask = new Task(inputName.getText().toString(), inputDescription.getText().toString(),
@@ -75,13 +57,8 @@ public class TaskView extends AppCompatActivity {
 
                     startActivity(new Intent(TaskView.this, MainActivity.class));
                     break;
-                case R.id.deleteBtn:
-                    //delete from the database using method defined in Main Activity
-                    startActivity(new Intent(TaskView.this, MainActivity.class));
-                    break;
             }
-
         }
     };
 }
->>>>>>> Tyler
+
