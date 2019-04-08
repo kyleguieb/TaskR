bb1bbc8fd0f9ba4ddce3ea4db0e579e8755761e8
tlrYere <tyler.yere772@myci.csuci.edu>
2019-04-08 13:12:45 -0700

    finished displaying actual task in TaskView haha

--- TaskR/app/src/main/java/android/bignerdranch/taskr/TaskView.java
+++ TaskR/app/src/main/java/android/bignerdranch/taskr/TaskView.java
@@ -6,16 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TaskView extends AppCompatActivity {
 
-    private String mTaskName;
+    private String mOriginalTaskName;
+    private String mOriginalDescription;
+    private String mOriginalDateAndTime;
     private static final String EXTRA_TASK_NAME = "com.bignerdranch.android.taskr.extra_task_name";
+    private static final String EXTRA_TASK_DESC = "com.bignerdranch.android.taskr.extra_task_description";
+    private static final String EXTRA_TASK_DATE_AND_TIME = "com.bignerdranch.android.taskr.extra_task_date_and_time";
 
     public static Intent newIntent(Context packageContext, String taskName, String taskDescription, String taskDateAndTime) {
         Intent intent = new Intent(packageContext, TaskView.class); //for retrieving task name
         intent.putExtra(EXTRA_TASK_NAME, taskName);
+        intent.putExtra(EXTRA_TASK_DESC, taskDescription);
+        intent.putExtra(EXTRA_TASK_DATE_AND_TIME, taskDateAndTime);
         return intent;
     }
 
@@ -23,7 +30,17 @@ public class TaskView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_view);
-        mTaskName = getIntent().getStringExtra(EXTRA_TASK_NAME);
+        mOriginalTaskName = getIntent().getStringExtra(EXTRA_TASK_NAME);
+        mOriginalDateAndTime = getIntent().getStringExtra(EXTRA_TASK_DATE_AND_TIME);
+        mOriginalDescription = getIntent().getStringExtra(EXTRA_TASK_DESC);
+
+        TextView displayTitle = (TextView) findViewById(R.id.taskViewTitle);
+        displayTitle.setText(mOriginalTaskName);
+        TextView displayDateNTime = (TextView) findViewById(R.id.taskViewDateNTime);
+        displayDateNTime.setText(mOriginalDateAndTime);
+        //String hardCodedDescription = "THIS IS THE DESCRIPTION OKAY";
+        TextView displayDesc = (TextView) findViewById(R.id.taskViewDescription);
+        displayDesc.setText(mOriginalDescription);
 
         defineButtons();
     }
@@ -54,15 +71,14 @@ public class TaskView extends AppCompatActivity {
                     }
 
                     Task alteredTask = new Task(inputName.getText().toString(), inputDescription.getText().toString(),
-                            inputDate.getText().toString() + " at " +
-                                    inputTime.getText().toString());
-                    MainActivity.updateTask(mTaskName, alteredTask);
+                            inputDate.getText().toString() + " at " + inputTime.getText().toString());
+                    MainActivity.updateTask(mOriginalTaskName, alteredTask);
                     startActivity(new Intent(TaskView.this, MainActivity.class));
 
                     break;
                 case R.id.deleteBtn:
                     //delete from the database using method defined in Main Activity
-                    MainActivity.deleteTask(mTaskName);
+                    MainActivity.deleteTask(mOriginalTaskName);
                     startActivity(new Intent(TaskView.this, MainActivity.class));
                     break;
             }
