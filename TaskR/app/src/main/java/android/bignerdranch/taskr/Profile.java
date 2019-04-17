25306fab70dc3243bff5418783cf54a553eaa13c
hiimkoi <hithereimkoi@gmail.com>
2019-04-17 15:24:29 -0700

    cleaned profile

--- TaskR/app/src/main/java/android/bignerdranch/taskr/Profile.java
+++ TaskR/app/src/main/java/android/bignerdranch/taskr/Profile.java
@@ -38,21 +38,8 @@ public class Profile extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        //defineButtons();
+
     }
 
-//    public void defineButtons() {
-//        findViewById(R.id.Friends_List_Button).setOnClickListener(buttonClickListener);
-//    }
-//
-//    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            switch(v.getId()) {
-//                case R.id.Friends_List_Button:
-//                    startActivity(new Intent(Profile.this, FriendList.class));
-//                    break;
-//            }
-//        }
-//    };
+
 }
