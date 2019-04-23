fa328e288bc69066f7cddb2d1e26bbbb5117e772
Tiffany Lamm <tiffany.lamm091@myci.csuci.edu>
2019-04-23 14:49:24 -0700

    In progress of implementing changing theme

--- TaskR/app/src/main/java/android/bignerdranch/taskr/Rewards.java
+++ TaskR/app/src/main/java/android/bignerdranch/taskr/Rewards.java
@@ -1,5 +1,6 @@
 package android.bignerdranch.taskr;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -7,8 +8,11 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Rewards extends AppCompatActivity {
+public class Rewards extends Activity implements OnClickListener {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -30,17 +34,68 @@ public class Rewards extends AppCompatActivity {
         }
     };
 
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_rewards);
-        //getTheme().applyStyle(R.style.AppThemeMint, true);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_rewards);
+
+        findViewById(R.id.setButton).setOnClickListener(this);
+        //findViewById(R.id.button2).setOnClickListener(this);
+        //findViewById(R.id.button3).setOnClickListener(this);
+
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        // TODO Auto-generated method stub
+        switch (v.getId())
+        {
+//            case R.id.button1:
+//                Utils.changeToTheme(this, Utils.THEME_DEFAULT);
+//                break;
+            case R.id.setButton:
+                Utils.changeToTheme(this, Utils.THEME_WHITE);
+                break;
+//            case R.id.button3:
+//                Utils.changeToTheme(this, Utils.THEME_BLUE);
+//                break;
+        }
+    }
+
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        //setContentView(R.layout.activity_rewards);
+////        final int theme = R.style.AppTheme;
+////        getTheme().applyStyle(theme, true);
+//        setContentView(R.layout.activity_rewards);
+//
+//        Button change = findViewById(R.id.setButton);
+//        change.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                getApplication().setTheme(R.style.AppThemeMint);
+////                recreate();
+////                theme = R.style.AppThemeMint;
+//                recreate();
+//
+//            }
+//        });
+//
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//
+//    }
 
 
 }
---
+++ TaskR/app/src/main/java/android/bignerdranch/taskr/Utils.java
@@ -0,0 +1,45 @@
+package android.bignerdranch.taskr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.app.Activity;
+import android.bignerdranch.taskr.R;
+
+public class Utils {
+
+// TODO: To be finished-ish eventually
+
+        private static int sTheme;
+        public final static int THEME_DEFAULT = 0;
+        public final static int THEME_WHITE = 1;
+        public final static int THEME_BLUE = 2;
+        /**
+         * Set the theme of the Activity, and restart it by creating a new Activity of the same type.
+         */
+        public static void changeToTheme(Activity activity, int theme)
+        {
+            sTheme = theme;
+            activity.finish();
+            activity.startActivity(new Intent(activity, activity.getClass()));
+        }
+        /** Set the theme of the activity, according to the configuration. */
+        public static void onActivityCreateSetTheme(Activity activity)
+        {
+            switch (sTheme)
+            {
+                default:
+                case THEME_DEFAULT:
+                    activity.setTheme(R.style.AppTheme);
+                    break;
+                case THEME_WHITE:
+                    activity.setTheme(R.style.AppThemeMint);
+                    break;
+                case THEME_BLUE:
+                    activity.setTheme(R.style.AppThemeLilac);
+                    break;
+            }
+        }
+
+
+}
+
